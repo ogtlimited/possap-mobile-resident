cbf10a9bc51442b7c3a6dc9668c9dce198ecde14
Khadija Ladan <khadija.ladan@outsourceglobal.com>
2023-01-11 12:07:24 +0100

    added search page

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,27 +1,42 @@
+import { LoadingController } from '@ionic/angular';
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ConferenceData } from 'src/app/providers/conference-data';
-import { baseEndpoints } from '../../../core/config/endpoints';
-import { RequestService } from '../../../core/request/request.service';
+import { RequestService } from 'src/app/core/request/request.service';
+import { baseEndpoints } from 'src/app/core/config/endpoints';
 
 @Component({
-  selector: 'app-request-details',
-  templateUrl: './request-details.component.html',
-  styleUrls: ['./request-details.component.scss'],
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
 })
-export class RequestDetailsComponent implements OnInit {
+export class SearchPage implements OnInit {
+  loading = false;
+
   request: any = {};
   approvers = [];
   approvalWorkflow = [];
+  hasData = false;
   private routeSub: Subscription;
   constructor(
     private route: ActivatedRoute,
     public confData: ConferenceData,
-    private reqS: RequestService
+    private reqS: RequestService,
+    private loader: LoadingController,
+    private router: Router,
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    const loading = await this.loader.create({
+      message: 'Loading...',
+      duration: 3000,
+      cssClass: 'custom-loading',
+    });
+
+    loading.present();
+
     this.routeSub = this.route.params.subscribe((params) => {
       console.log(params);
       this.fetchData(params.id);
@@ -32,11 +47,16 @@ export class RequestDetailsComponent implements OnInit {
     this.reqS.get(baseEndpoints.requests + '/' + id).subscribe((res: any) => {
       console.log('testt', res.data);
       this.request = res.data;
+      this.hasData = true;
       this.approvers = res.data.service.workflow[0].WorkFlowApprovalLevel;
       // this.approvalWorkflow = [
       //   ...res.data.service.approvalWorkFlow,
       //   'Completed',
       // ];
     });
+  }
+
+  backToHome(){
+    this.router.navigate(['/']);
   }
 }
