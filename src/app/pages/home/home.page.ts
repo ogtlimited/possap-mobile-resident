bca68bbd0c81bbbe865f23d73b512e2d8bb8d002
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-03-30 09:44:01 +0100

    bank3d integration

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,26 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ConferenceData } from 'src/app/providers/conference-data';
 import { SearchComponent } from 'src/app/components/search/search.component';
-
+import { Bank3dPaymentService } from 'Bank3d-Payment-Angular';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
   speakers: any[] = [];
   searchTerm;
   slideOpts = {
     initialSlide: 0,
     speed: 400,
   };
-
+  merchantKey = 'your-merchant-key';
+  reference = 'REF-' + Math.random().toString(16).slice(2);
+  amount = '5000';
   constructor(
     public confData: ConferenceData,
     private router: Router,
     private modalController: ModalController,
-    ) {}
+    private bank3d: Bank3dPaymentService
+  ) {}
 
   ionViewDidEnter() {
     this.confData.getSpeakers().subscribe((speakers: any[]) => {
@@ -33,10 +35,10 @@ export class HomePage implements OnInit {
 
   ngOnInit() {}
 
-  getBg(num){
+  getBg(num) {
     return `url(assets/img/home/img${num + 1}.png)`;
   }
-  submit(){
+  submit() {
     this.router.navigate([`search/${this.searchTerm}`]);
   }
 
@@ -47,5 +49,10 @@ export class HomePage implements OnInit {
     });
     await modal.present();
   }
-
+  paymentCallback(response): void {
+    console.log('Pay', response);
+  }
+  closedPaymentModal(): void {
+    console.log('payment is closed');
+  }
 }
